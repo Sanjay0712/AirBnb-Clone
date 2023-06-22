706cbf5f30ccaaaf5210b4101b317e8bdef1e447
Sanjay0712 <130839721+Sanjay0712@users.noreply.github.com>
2023-06-22 12:43:54 +0530

    Update Logo.tsx

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -10,7 +10,7 @@ const Logo = () => {
     <Image
       onClick={() => router.push('/')}
       className="hidden md:block cursor-pointer" 
-      src="/images/logo.png" 
+      src="/images/logo1.png" 
       height="100" 
       width="100" 
       alt="Logo" 
